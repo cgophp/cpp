3a37ee4f2f01bfa0ad5add47f79bb187640fec7e
cgophp <cgophp@gmail.com>
2023-01-18 02:20:43 +0800

    tp6

--- admin/app/common.php
+++ admin/app/common.php
@@ -1,2 +1,31 @@
 <?php
+
 // 应用公共文件
+
+use fast\Http as FastHttpClient;
+
+function func_http_get($url, $params = [], $options = [])
+{
+    return FastHttpClient::get($url, $params, $options);
+}
+
+function func_http_post($url, $params = [], $options = [])
+{
+    return FastHttpClient::post($url, $params, $options);
+}
+
+function func_date_time($timestamp = 'now')
+{
+    $format = 'Y-m-d H:i:s';
+
+    if ($timestamp === 'now') {
+        return date($format);
+    }
+
+    $timestamp = intval($timestamp);
+    if ($timestamp > 0) {
+        return date($format, $timestamp);
+    }
+
+    return '';
+}
